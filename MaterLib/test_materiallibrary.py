8a333a5681a05a430b86e14c71de0e12fb99a0b4
lui ziyu <iopuioyui@outlook.com>
2025-03-04 18:19:12 +0800

    MaterLib\testshili\吒儿.flac

--- MaterLib/test_materiallibrary.py
+++ MaterLib/test_materiallibrary.py
@@ -1,6 +1,11 @@
 import pytest
 import MaterLib as ML
+import sqlalchemy
+
+sqlalchemy.create_engine("sqlite:///example.db")
 
 
 def test_init():
-    ML.letit_go()
+    ML.letit_go(r"\testshili\bilibili_BV1yx411A72S_MP3.mp4")
+
+    ML.letit_go(r"\testshili\吒儿.flac", local=True)
